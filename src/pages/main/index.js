02e0fb510fd3362f9d118415f64963e518a4d4c5
IncinerateZ <inferious77@gmail.com>
2023-11-23 15:19:52 +0900

    fix: distance check

--- src/pages/main/index.js
+++ src/pages/main/index.js
@@ -161,8 +161,8 @@ window.onload = async () => {
     let lng = 127.4468659;
     options.center = new kakao.maps.LatLng(lat, lng);
 
-    prevLL = { ...LL };
     LL = { lat: lat, lng: lng };
+    prevLL = { ...LL };
     _kakaoLL = options.center;
 
     map = new kakao.maps.Map(container, options); //지도 생성 및 객체 리턴
@@ -177,19 +177,20 @@ window.onload = async () => {
 
     kakao.maps.event.addListener(map, 'dragstart', function () {
         lockView = false;
-
-        map.setCenter();
     });
 
     drawDirectionPaths('맘스터치 우송대점');
 
     setInterval(() => {
+        console.log('c');
         if (
             prevLL &&
             Math.abs(prevLL.lat - LL.lat) < 0.0002 &&
             Math.abs(prevLL.lng - LL.lng) < 0.0002
         )
             return;
+        prevLL = { ...LL };
+
         drawDirectionPaths('맘스터치 우송대점');
     }, 5000);
 };
