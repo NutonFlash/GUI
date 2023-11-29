41192980fd681f872320eadebe9d50296f90a7bc
NutonFlash <aakislitsin@gmail.com>
2023-11-30 02:35:35 +0900

    fix camera page

---
+++ src/pages/camera/index.js
@@ -0,0 +1,7 @@
+function goHome() {
+    t = document.createElement('a');
+    t.href = '../main/index.html';
+    t.click();;
+    // Add your home button functionality here, for example, redirect to the home page.
+    alert("Redirecting to home page!");
+}
